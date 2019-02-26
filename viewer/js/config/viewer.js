eb781d1793f61bb24016b8ecacfd08f10cf56a55
Bill Beers <bill.beers@aecom.com>
2019-02-26 09:06:00 -0500

    Refining the LODs to avoid zooming out to the whole planet.

--- viewer/js/config/viewer.js
+++ viewer/js/config/viewer.js
@@ -130,8 +130,101 @@ define([
         mapOptions: {
             basemap: 'streets',
             center: [-84.380741, 28.190003],
-            zoom: 6,
-            sliderStyle: 'small'/*,
+            zoom: 1, //use 7 if using default lods; this is the index of the lods listed below,
+            sliderStyle: 'small',
+            lods: [
+                {
+                    "level": 6,
+                    "resolution": 2445.98490512499,
+                    "scale": 9244648.868618
+                },
+                {
+                    'level': 7,
+                    'resolution': 1222.992452562495,
+                    'scale': 4622324.434309
+                },
+                {
+                    'level': 8,
+                    'resolution': 611.4962262813797,
+                    'scale': 2311162.217155
+                },
+                {
+                    'level': 9,
+                    'resolution': 305.74811314055756,
+                    'scale': 1155581.108577
+                },
+                {
+                    'level': 10,
+                    'resolution': 152.87405657041106,
+                    'scale': 577790.554289
+                },
+                {
+                    'level': 11,
+                    'resolution': 76.43702828507324,
+                    'scale': 288895.277144
+                },
+                {
+                    'level': 12,
+                    'resolution': 38.21851414253662,
+                    'scale': 144447.638572
+                },
+                {
+                    'level': 13,
+                    'resolution': 19.10925707126831,
+                    'scale': 72223.819286
+                },
+                {
+                    'level': 14,
+                    'resolution': 9.554628535634155,
+                    'scale': 36111.909643
+                },
+                {
+                    'level': 15,
+                    'resolution': 4.77731426794937,
+                    'scale': 18055.954822
+                },
+                {
+                    'level': 16,
+                    'resolution': 2.388657133974685,
+                    'scale': 9027.977411
+                },
+                {
+                    'level': 17,
+                    'resolution': 1.1943285668550503,
+                    'scale': 4513.988705
+                },
+                {
+                    'level': 18,
+                    'resolution': 0.5971642835598172,
+                    'scale': 2256.994353
+                },
+                {
+                    'level': 19,
+                    'resolution': 0.29858214164761665,
+                    'scale': 1128.497176
+                },
+                {
+                    'level': 20,
+                    'resolution': 0.14929107082380833,
+                    'scale': 564.248588
+                },
+                {
+                    'level': 21,
+                    'resolution': 0.07464553541190416,
+                    'scale': 282.124294
+                },
+                {
+                    "level": 22,
+                    "resolution": 0.03732276770595208,
+                    "scale": 141.062147
+                },
+                {
+                    "level": 23,
+                    "resolution": 0.01866138385297604,
+                    "scale": 70.5310735
+                }
+            ]
+            /*,
             titles: {
                 header: 'Environmental Screening Tool',
                 subHeader: 'Map Viewer',
