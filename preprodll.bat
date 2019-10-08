1597ba64e73eb33bdd103a1037560b2c37bb6b98
Bill Beers <bill.beers@aecom.com>
2019-10-08 16:06:26 -0400

    Updates to batch files to publishing just layer loader config.

---
+++ preprodll.bat
@@ -0,0 +1,1 @@
+"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfigurator\bin\Release\layerLoaderConfigurator.exe" stage "c:\temp\layerLoader.js" "pandora.est.vpn"
