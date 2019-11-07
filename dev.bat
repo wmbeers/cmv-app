e6e6d09f5e9a7b9d48a87f5e4150dc07ac5ee1e2
Bill Beers <bill.beers@aecom.com>
2019-11-07 09:37:31 -0500

    Deployment fix

--- dev.bat
+++ dev.bat
@@ -1,2 +1,2 @@
-"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfigurator\bin\Release\layerLoaderConfigurator.exe" dev "viewer\js\config\layerLoader.js"
+"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfigurator\bin\Release\layerLoaderConfigurator.exe" dev viewer\js\config\
 grunt build-deploy --target=dev
