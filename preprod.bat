66cad734e9a17f286a455dac446946eba9a0e5c8
Bill Beers <bill.beers@aecom.com>
2019-11-12 14:37:10 -0500

    Update preprod.bat
    Fixing preprod deployment

--- preprod.bat
+++ preprod.bat
@@ -1,2 +1,2 @@
-"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfigurator\bin\Release\layerLoaderConfigurator.exe" stage "viewer\js\config\layerLoader.js"
+"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfigurator\bin\Release\layerLoaderConfigurator.exe" stage viewer\js\config\
 grunt build-deploy --target=preprod
