317f1f801f62576c22429f5e2bf8c18147c77ba1
Bill Beers <bill.beers@aecom.com>
2019-12-16 10:55:06 -0500

    Polyfill for String.startsWith
    Responding to user testing, String.startsWith not supported in IE. Added a polyfill method.

--- viewer/js/arrayFind.js
+++ viewer/js/arrayFind.js
@@ -44,3 +44,12 @@ if (!Array.prototype.find) {
         writable: true
     });
 }
+
+//https://stackoverflow.com/questions/30867172/code-not-running-in-ie-11-works-fine-in-chrome
+if (!String.prototype.startsWith) {
+    //eslint-disable-next-line no-extend-native
+    String.prototype.startsWith = function (searchString, position) {
+        position = position || 0;
+        return this.indexOf(searchString, position) === position;
+    };
+}
