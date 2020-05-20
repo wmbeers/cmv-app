41a45c85388c0cb6b0f2601c2250d997bd89068b
Bill Beers <bill.beers@aecom.com>
2020-05-20 11:55:20 -0400

    Shortcuts for updating layerLoader.js and projects.js config files with new .Net Core version of LayerLoaderConfigurator.

---
+++ prodllcore.bat
@@ -0,0 +1,1 @@
+"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfiguratorCore\bin\Release\netcoreapp3.1\layerLoaderConfiguratorCore.exe" prod - "estlapp05.geoplan.ufl.edu"
