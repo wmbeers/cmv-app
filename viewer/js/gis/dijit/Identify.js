3a768dd1695fb59618dc26be48cc3bf34abad7c0
Bill Beers <bill.beers@aecom.com>
2020-06-25 16:03:33 -0400

    Treating layer.capabilities properly, as a string, not an array.

--- viewer/js/gis/dijit/Identify.js
+++ viewer/js/gis/dijit/Identify.js
@@ -127,7 +127,8 @@ define([
                     // Identify (Feature Service), create an
                     // infoTemplate for the graphic features. Create
                     // it only if one does not already exist.
-                    if (layer.capabilities && array.indexOf(layer.capabilities.toLowerCase(), 'data') < 0) {
+                    //NOPE if (layer.capabilities && array.indexOf(layer.capabilities.toLowerCase(), 'data') < 0) {
+                    if (layer.capabilities && layer.capabilities.toLowerCase().indexOf('data') < 0) {
                         if (!layer.infoTemplate) {
                             infoTemplate = this.getInfoTemplate(layer, layer.layerId);
                             if (infoTemplate) {
@@ -247,6 +248,7 @@ define([
             }
         },
         executeIdentifyTask: function (evt) {
+            debugger;
             var mapPoint = evt.mapPoint;
             var identifyParams = this.createIdentifyParams(mapPoint);
             var identifies = [];
