abadd73b047f5c8f66b252962a025be5b0902541
Bill Beers <bill.beers@aecom.com>
2020-07-21 12:15:54 -0400

    Updating stage deployment script.

--- stage.bat
+++ stage.bat
@@ -1,2 +1,2 @@
-"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfigurator\bin\Release\layerLoaderConfigurator.exe" stage viewer\js\config\
+"C:\Users\bill.beers\Documents\Visual Studio Projects\LayerLoaderConfigurator\LayerLoaderConfiguratorCore\bin\Release\netcoreapp3.1\layerLoaderConfigurator.exe"  stage viewer\js\config\
 grunt build-deploy --target=stagepoke
